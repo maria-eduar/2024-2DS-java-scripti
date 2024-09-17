f616f685996b42aa0f6a87beac57f443838d440c
Maria Eduarda Ramirez Daniel <160872621+maria-eduar@users.noreply.github.com>
2024-09-17 12:10:15 -0300

    Add files via upload

---
+++ java/variaveis.js
@@ -0,0 +1,24 @@
+//tipo nunmber
+
+
+// var era utilizado para todas as variaveis
+const numero01 = 3
+// let e uma atualização do var e é passivel de variação 
+const numero02 = 10
+
+// const é uma maneira de declarar uma variavel que irá permanecer constante
+const numero03 = 7
+
+const soma = numero01 + numero02
+const subtração = numero02 - numero01
+const mutiplicacao = numero03 * numero02
+const divisao = numero02 / numero01
+
+console.log(soma)
+console.log(subtração)
+console.log(multiplicacao)
+console.log(divisao)
+
+
+
+
